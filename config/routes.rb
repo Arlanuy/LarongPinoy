6f3d465fb2cee125fd111521a6e5e8e71f2b2de9
Menard Ysrael Cruz <ysrael214@gmail.com>
2019-10-04 06:02:35 +0800

    reroute home

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :questions
   resources :quizzes
 
-  root 'pages#home'
+  root 'pages#home', as: 'home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
