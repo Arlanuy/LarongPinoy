b3b00983ce21504ed2c3bb2555c82350fdb65637
Arlan <arlanvincentuy@gmail.com>
2019-10-06 22:53:51 +0800

    update user scaffold to user devise implementation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
-  has_secure_password
-  validates :name, uniqueness: true, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 end
